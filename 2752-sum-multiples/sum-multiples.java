71948d70474a2f84ac6d7ca6031ad58b9f2df187
Ayush Gupta <ayush2006gupta@gmail.com>
2026-08-09 21:36:40 +0530

    Time: 5 ms (1.51%) | Memory: 46.6 MB (9.58%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -1,17 +1,30 @@
 class Solution {
     public int sumOfMultiples(int n) {
-        int[] arr = new int[n];
-        int j=0;
-        int sum=0;
+        // int[] arr = new int[n];
+        // int j=0;
+        // int sum=0;
+        // for(int i=1;i<=n;i++){
+        //     if(i%3==0 || i%5==0 || i%7==0){
+        //         arr[j] = i;
+        //         j++;
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     sum += arr[i];
+        // }
+
+        List<Integer> list = new ArrayList<>();
+        int sum = 0;
         for(int i=1;i<=n;i++){
             if(i%3==0 || i%5==0 || i%7==0){
-                arr[j] = i;
-                j++;
+                list.add(i);
             }
         }
-        for(int i=0;i<n;i++){
-            sum += arr[i];
+
+        for(int i=0;i<list.size();i++){
+            sum += list.get(i);
         }
+
         return sum;
     }
 }
